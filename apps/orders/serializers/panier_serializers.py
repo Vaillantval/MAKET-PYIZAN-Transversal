eceb62d5ff4d044ddf73f9952aff6bcabf52031f
V. Vaillant <vaillantval1999@gmail.com>
2026-03-29 14:21:32 -0400

    message de contact

--- apps/orders/serializers/panier_serializers.py
+++ apps/orders/serializers/panier_serializers.py
@@ -4,26 +4,31 @@ from apps.orders.models import Panier, LignePanier
 
 class LignePanierSerializer(serializers.ModelSerializer):
     """Ligne de panier enrichie avec les infos produit."""
-    slug           = serializers.CharField(source='produit.slug')
-    nom            = serializers.CharField(source='produit.nom')
-    prix_unitaire  = serializers.DecimalField(
-                       source='produit.prix_unitaire',
-                       max_digits=10, decimal_places=2,
-                     )
-    unite_vente    = serializers.CharField(source='produit.unite_vente')
-    producteur_id  = serializers.IntegerField(source='produit.producteur.pk')
-    producteur_nom = serializers.SerializerMethodField()
-    image          = serializers.SerializerMethodField()
-    stock_reel     = serializers.IntegerField(source='produit.stock_reel')
+    slug              = serializers.CharField(source='produit.slug')
+    nom               = serializers.CharField(source='produit.nom')
+    prix_unitaire     = serializers.DecimalField(
+                          source='produit.prix_unitaire',
+                          max_digits=10, decimal_places=2,
+                        )
+    unite_vente       = serializers.CharField(source='produit.unite_vente')
+    unite_vente_label = serializers.SerializerMethodField()
+    producteur_id     = serializers.IntegerField(source='produit.producteur.pk')
+    producteur_nom    = serializers.SerializerMethodField()
+    image             = serializers.SerializerMethodField()
+    stock_reel        = serializers.IntegerField(source='produit.stock_reel')
 
     class Meta:
         model  = LignePanier
         fields = [
             'id', 'slug', 'nom', 'quantite',
             'prix_unitaire', 'sous_total',
-            'unite_vente', 'producteur_id',
-            'producteur_nom', 'image', 'stock_reel',
+            'unite_vente', 'unite_vente_label',
+            'producteur_id', 'producteur_nom',
+            'image', 'stock_reel',
         ]
+
+    def get_unite_vente_label(self, obj):
+        return obj.produit.get_unite_vente_display()
 
     def get_producteur_nom(self, obj):
         return obj.produit.producteur.user.get_full_name()
